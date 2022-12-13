75e647d87e7889ec7e8a3653ca5cacbc2df1c02c
RZhang <richardzhiyuanzhang@gmail.com>
2022-12-13 17:23:41 -0500

    update serializers to include all model fields with read_only and write_only attributes set for user serializer

--- server/receipts/serializers.py
+++ server/receipts/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.fields import CharField
 
 from .models import User, Receipt, Tag
 
@@ -6,14 +7,18 @@ from .models import User, Receipt, Tag
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email')
+        fields = ('url', 'id', 'email', 'password')
+
+        read_only_fields = ('id',)
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 8, 'max_length': 64}}
+
 
 class ReceiptSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Receipt
-        fields = ('date', 'image')
+        fields = ('url', 'id', 'date', 'image', 'user')
 
 class TagSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Tag
-        fields = ('tag',)
+        fields = ('url', 'id', 'tag', 'receipts')
