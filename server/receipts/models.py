4137a2cfcc9deff39ee304de29c70fa7797c2ff3
RZhang <richardzhiyuanzhang@gmail.com>
2022-12-19 15:29:12 -0500

    update models: remove user model, add relation with auth.User model in Receipt model. Rename and update settings for each field in Receipt model. Adjust ordering of data in Receipt and Tag model. Update serializers: add hyperlinked relations between models, add additional serializer fields, add password validation, change password to write_only.

--- server/receipts/models.py
+++ server/receipts/models.py
@@ -2,24 +2,25 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=256)
-
-    def __str__(self):
-        return self.email
-
 class Receipt(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    creator = models.ForeignKey('auth.User', related_name='receipts', on_delete=models.CASCADE)
+    title = models.CharField(max_length=500, blank=True, null=True, default='')
     date = models.DateField()
-    image = models.ImageField(upload_to='images')
+    receiptImage = models.ImageField(upload_to='images')
+
+    class meta:
+        ordering = ['title', 'date']
 
     def __str__(self):
-        return self.date.strftime("%m/%d/%Y")
+        return f'{self.title or ""} {self.date.strftime("%m/%d/%Y")}'
+
 
 class Tag(models.Model):
     receipts = models.ManyToManyField(Receipt, blank=True)
     tag = models.CharField(max_length=200)
+
+    class meta:
+        ordering = ['tag']
 
     def __str__(self):
         return self.tag
--- server/receipts/serializers.py
+++ server/receipts/serializers.py
@@ -2,24 +2,45 @@ from rest_framework import serializers
 from rest_framework.fields import CharField
 
 from .models import User, Receipt, Tag
+from django.contrib.auth.models import User
+
+from django.contrib.auth import password_validation
+from django.contrib.auth.hashers import make_password
+from rest_framework.validators import UniqueValidator
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    receipts = serializers.HyperlinkedRelatedField(
+        many=True, view_name='receipt-detail', read_only=True)
+    email = serializers.EmailField(required=True, validators=[
+                                   UniqueValidator(queryset=User.objects.all())])
+    password = serializers.CharField(
+        help_text=password_validation.password_validators_help_text_html(), write_only=True)
+
     class Meta:
         model = User
-        fields = ('url', 'id', 'email', 'password')
+        fields = ('url', 'id', 'username', 'email', 'password', 'receipts')
 
-        read_only_fields = ('id',)
-        extra_kwargs = {'password': {'write_only': True, 'min_length': 8, 'max_length': 64}}
+    def validate_password(self, password):
+        user = self.context['request'].user
+        password_validation.validate_password(password=password, user=user)
+
+        return make_password(password)
 
 
 class ReceiptSerializer(serializers.HyperlinkedModelSerializer):
+    creatorUrl = serializers.ReadOnlyField(source='creator.url')
+    creator = serializers.ReadOnlyField(source='creator.username')
+    tags = serializers.HyperlinkedRelatedField(
+        many=True, view_name='tag-detail', queryset=Tag.objects.all())
+
     class Meta:
         model = Receipt
-        fields = ('url', 'id', 'date', 'image', 'user')
+        fields = ('url', 'id', 'title', 'date',
+                  'receiptImage', 'tags', 'creatorUrl', 'creator')
+
 
 class TagSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Tag
         fields = ('url', 'id', 'tag', 'receipts')
-
